a4506cebd5596bc60b607763d6a8e35ea68678e9
dungeon2567 <31074903+dungeon2567@users.noreply.github.com>
2025-01-12 23:06:10 +0000

    test

--- auth.ts
+++ auth.ts
@@ -3,5 +3,5 @@ import { D1Adapter } from "@auth/d1-adapter"
  
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [],
-  adapter: D1Adapter(env.db),
+  adapter: D1Adapter(process.env.db),
 })
