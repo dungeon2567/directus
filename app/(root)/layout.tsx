665ac7ae8ce1c7aff22984c116a2fae7f9caf6e7
DIEGO\diego <diego.braga.guedes@outlook.com>
2025-01-21 17:38:18 -0300

    react query

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,8 +8,44 @@ import { HeaderMegaMenu } from '@/components/HeaderMegaMenu/HeaderMegaMenu';
 import Logo from './Logo';
 import { SessionContextProvider } from '@/components/SessionContext';
 import { createClient } from '@/utils/supabase/client';
+import { PostgrestFilterBuilder } from '@supabase/postgrest-js';
+
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+  QueryKey
+} from '@tanstack/react-query'
 
 const client = createClient();
+
+// Define a default query function that will receive the query key
+const defaultQueryFn = async ({ queryKey }: { queryKey: QueryKey }) => {
+  const config = queryKey[1] as any;
+
+  const source = client.from(`${queryKey[0]}`)
+      .select(config.columns, config.options)
+
+  const { count, data, error } = await (config.filter
+      ? config.filter(source)
+      : source);
+
+  if(error)
+    throw error;
+
+  return data;
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn,
+    },
+  },
+});
+
 
 
 export default function RootLayout({ children }: { children: any }) {
@@ -17,36 +53,38 @@ export default function RootLayout({ children }: { children: any }) {
 
   return (
     <SessionContextProvider supabaseClient={client}>
-      <AppShell
-        header={{ height: 60 }}
+      <QueryClientProvider client={queryClient}>
+        <AppShell
+          header={{ height: 60 }}
 
-        transitionDuration={500}
-        transitionTimingFunction="ease"
-        navbar={{
-          width: 300,
-          breakpoint: 'sm',
-          collapsed: { mobile: !opened },
-        }}
-        padding="md"
-      >
-        <AppShell.Header>
-          <Group justify="space-between" pl="xl" pr="xl" h="100%">
-            <Burger
-              opened={opened}
-              onClick={toggle}
-              hiddenFrom="sm"
-              size="md"
-            />
-            <Logo width={36} height={36} color="#ff6347" />
-            <HeaderMegaMenu />
-          </Group>
-        </AppShell.Header>
+          transitionDuration={500}
+          transitionTimingFunction="ease"
+          navbar={{
+            width: 300,
+            breakpoint: 'sm',
+            collapsed: { mobile: !opened },
+          }}
+          padding="md"
+        >
+          <AppShell.Header>
+            <Group justify="space-between" pl="xl" pr="xl" h="100%">
+              <Burger
+                opened={opened}
+                onClick={toggle}
+                hiddenFrom="sm"
+                size="md"
+              />
+              <Logo width={36} height={36} color="#ff6347" />
+              <HeaderMegaMenu />
+            </Group>
+          </AppShell.Header>
 
-        <AppShell.Navbar>
-          <NavbarNested />
-        </AppShell.Navbar>
+          <AppShell.Navbar>
+            <NavbarNested />
+          </AppShell.Navbar>
 
-        <AppShell.Main>          {children}</AppShell.Main>
-      </AppShell>
+          <AppShell.Main>          {children}</AppShell.Main>
+        </AppShell>
+      </QueryClientProvider>
     </SessionContextProvider>);
 }
